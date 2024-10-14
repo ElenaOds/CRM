ae895408b84eab7dad4bccde8ce51df2fa5887ab
Olena <elpg.ods@gmail.com>
2024-10-14 17:03:08 +0300

    basename is changed

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>  
-    <BrowserRouter basename="crm">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
     </Provider>
